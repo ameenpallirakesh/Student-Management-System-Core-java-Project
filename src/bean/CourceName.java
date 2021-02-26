6d85175564021d314d07b85cc86720845d4efa62
ameenpallirakesh <79705900+ameenpallirakesh@users.noreply.github.com>
2021-02-26 19:33:42 +0530

    Add files via upload

---
+++ src/bean/CourceName.java
@@ -0,0 +1,16 @@
+package bean;
+
+public class CourceName {
+	/**
+	 *CourseName Class With Data Members And Getter,Setters 
+	 **/
+	 private String[] courceName={"C","Java","J2EE","Spring","Hibernate"};
+
+	public String[] getCourceName() {
+		return courceName;
+	}
+
+	public void setCourceName(String[] courceName) {
+		this.courceName = courceName;
+	}	
+}
---
+++ src/bean/CourseName.java
@@ -0,0 +1,14 @@
+package bean;
+
+public class CourseName {
+
+	 private String[] courceName={"C","Java","J2EE","Spring","Hibernate"};
+
+	public String[] getCourceName() {
+		return courceName;
+	}
+
+	public void setCourceName(String[] courceName) {
+		this.courceName = courceName;
+	}	
+}
